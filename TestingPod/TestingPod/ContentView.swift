a02b8b1abcba6c159f92704f5da5cd3a3115ce14
Konstantine Nikka-Sher Piterman <nikka@Konstantines-Mac-mini.local>
2020-04-23 11:34:56 -0700

    first commit

---
+++ TestingPod/TestingPod/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestingPod
+//
+//  Created by Konstantine Nikka-Sher Piterman on 12/21/19.
+//  Copyright © 2019 Konstantine Nikka-Sher Piterman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftytest.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+if let s = String.init("some string")? {
+    print (s)
+}
