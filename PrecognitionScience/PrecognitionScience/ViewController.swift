374643f7d4c2132fe778e0865ac9ea2f2b22e225
Konstantine Nikka-Sher Piterman <nikka@Konstantines-Mac-mini.local>
2020-04-23 13:23:28 -0700

    Psychic test A/B

--- PrecognitionScience/PrecognitionScience/ViewController.swift
+++ PrecognitionScience/PrecognitionScience/ViewController.swift
@@ -10,26 +10,71 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var correct: Int = 0
 
+    var incorrect: Int = 0
+    
+    var prediction: String!
+    
+    var result: String!
+    
+    @IBOutlet weak var correctGuesses: UILabel!
+    @IBOutlet weak var incorrectGuesses: UILabel!
+    
+    
     @IBOutlet weak var randomLetter: UILabel!
     
         @IBOutlet weak var yourGuess: UILabel!
     
     @IBAction func aPressed(_ sender: Any) {
-        yourGuess.text = "Prediction: A"
+        yourGuess.text = "A"
         
-      
+        numberDraw()
+
+        prediction = "A"
+         checkPrediction(prediction: prediction, result: result)
         
+        correctGuesses.text = "\(correct)"
+        incorrectGuesses.text = "\(incorrect)"
     }
     
     @IBAction func bPressed(_ sender: Any) {
         
-        yourGuess.text = "Prediction: B"
+        yourGuess.text = "B"
+        numberDraw()
+ 
+        prediction = "B"
+                checkPrediction(prediction: prediction, result: result)
+        
+        correctGuesses.text = "\(correct)"
+              incorrectGuesses.text = "\(incorrect)"
     }
     
     
     func numberDraw() {
+  
+        let options = ["A", "B"]
+        let randomOption = options.randomElement()!
+               result = randomOption
+        randomLetter.text = result
         
+
+        
+        print(randomOption)
+
+      print(correct)
+        print(incorrect)
+        
+    }
+    
+    
+    func checkPrediction(prediction: String, result: String) {
+
+        if prediction == result {
+            correct = correct + 1
+        } else {
+            incorrect = incorrect + 1
+        }
         
     }
 
@@ -38,6 +83,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
